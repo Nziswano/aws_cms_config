fda0ffc282378790e12e1f100143b6638a94d14f
Johan Martin <martin.johan@nziswano.co.za>
2022-02-13 13:33:28 +0200

    add hitcounter

--- lib/aws_cms_config-stack.ts
+++ lib/aws_cms_config-stack.ts
@@ -3,6 +3,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCmsConfigStack extends Stack {
@@ -15,8 +16,12 @@ export class AwsCmsConfigStack extends Stack {
       handler: 'hello.handler'
     });
 
+    const HelloWithHitCounter = new HitCounter(this, 'HolloHitCounter', {
+      downstream: hello
+    });
+
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: HelloWithHitCounter.handler
     });
   }
 }
